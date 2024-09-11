8db2b259c35aa7827f6a81eeb12fb7b34db24583
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-09-11 15:51:10 +0800

    upd

--- src/libs/Report/Report.tsx
+++ src/libs/Report/Report.tsx
@@ -1,10 +1,28 @@
 import { ReportProps } from "./types.ts";
 import SummaryHeader from "./components/SummaryHeader.tsx";
+import { useEffect, useState } from "react";
+import { codeToHtml } from "shiki";
 
 function Editor({ className }: ReportProps) {
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    const code = "const a = 1"; // 输入代码片段
+    codeToHtml(code, {
+      lang: "javascript",
+      theme: "vitesse-dark",
+    }).then((r) => {
+      setValue(r);
+    });
+  }, []);
   return (
     <div className={className}>
       <SummaryHeader />
+      <div
+        dangerouslySetInnerHTML={{
+          __html: value,
+        }}
+      ></div>
     </div>
   );
 }
