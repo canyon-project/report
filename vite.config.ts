50a295b28514f0265a01d22a5cbfc4d106e0633f
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-09-11 15:44:46 +0800

    upd

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react({}), dts()],
-  base: "/canyon-report/",
+  base: "/report/",
   build:
     process.env.USE_DTS_PLUGIN === "true"
       ? {
