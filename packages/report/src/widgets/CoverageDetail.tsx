16cb33aa32b860af7baa5f5630a7af536b00254a
Travis Zhang <wr.zhang25@gmail.com>
2025-12-12 12:01:33 +0800

    chore: daily development

--- packages/report/src/widgets/CoverageDetail.tsx
+++ packages/report/src/widgets/CoverageDetail.tsx
@@ -1,13 +1,26 @@
 
-import {Button} from "antd";
-const CoverageDetail = () => {
+import {initCanyonReportCore} from '@canyonjs/report-core'
+import {useEffect, useRef} from "react";
+function CanyonReport() {
+  const ref = useRef(null)
+  useEffect(() => {
+    if (ref.current){
+      initCanyonReportCore(ref.current,{
+        value: 'const a = 1',
+        language: 'javascript',
+        theme: 'vs-dark',
+      })
+    }
+
+  }, []);
   return (
     <div>
-      <Button>
-        我是CoverageDetail里的按钮
-      </Button>
-    </div>
-  );
-};
+      乌鸦坐飞机
+      <div ref={ref} style={{height:'300px',border:'1px solid #000'}}>
 
-export default CoverageDetail;
+      </div>
+    </div>
+  )
+}
+
+export default CanyonReport
