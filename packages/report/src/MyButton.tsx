460f1350ab9ac7d5441bca1075b10b618c388cf6
Travis Zhang <wr.zhang25@gmail.com>
2025-12-12 11:23:16 +0800

    chore: daily development

--- packages/report/src/MyButton.tsx
+++ packages/report/src/MyButton.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import {Button} from "antd";
 
 interface MyButtonProps {
   type?: 'primary'
@@ -7,10 +8,9 @@ interface MyButtonProps {
 export const MyButton: React.FC<MyButtonProps> = ({ type }) => {
   const [count, setCount] = useState(0)
   return (
-    <button className="my-button" onClick={() => setCount(count + 1)}>
-      my button
-      <br /> type: {type}
-      <br /> count: {count}
-    </button>
+    <div>
+      antd额按钮
+      <Button>nihao</Button>
+    </div>
   )
 }
